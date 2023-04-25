8a18f81c9dff756cdec103e1964ab16db647abbd
Ricky Alvarez <alvarez1@kenyon.edu>
2023-04-25 06:06:59 +0000

    new index

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,20 +1,45 @@
 const adminModel = require("../models/adminModel");
 const jwt = require("jsonwebtoken");
 
+exports.index = async (req, res) => {
+    if (req.cookies.access_token) {
+        try {
+            const admin = jwt.verify(req.cookies.access_token, process.env.JWT_SECRET);
+
+            return res.render('adminPanel', { 
+                error: null,
+                isAdmin: true, 
+                admin: admin, 
+            });
+        } catch {
+            return res.render('adminPanel', { 
+                error: 'Invalid token. Please login again.',
+                isAdmin: false, 
+            });
+        }
+    }
+
+    return res.render('adminPanel', { 
+        isAdmin: false, 
+        error: null
+    });
+};
+
 exports.login = async (req, res) => {
     const { username, password } = req.body;
+
     try {
         const admin = await adminModel.login(username, password);
 
         if (admin instanceof adminModel.Admin) {
             const token = jwt.sign(JSON.stringify(admin), process.env.JWT_SECRET);
+
             return res.cookie("access_token", token, {
                 httpOnly: true,
                 secure: process.env.NODE_ENV === "production",
-            }).redirect("/admin");
-        }
-        else
-        {
+            })
+            .redirect("/admin");
+        } else {
             return res.render(`adminPanel`, { isAdmin: false, error: admin });
         }
     } catch (error) {
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,20 +4,9 @@ const adminController = require('../controllers/adminController');
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 
-router.get('/', (req, res) => {
-    if (req.cookies.access_token) {
-        try {
-            const admin = jwt.verify(req.cookies.access_token, process.env.JWT_SECRET);
-            return res.render('adminPanel', { isAdmin: true, admin: admin });
-        } catch {
-            return res.render('adminPanel', { isAdmin: false, });
-        }
-    }
-    return res.render('adminPanel', { isAdmin: false, });
-});
+router.get('/', adminController.index);
 
 router.post('/login', adminController.login);
-
 router.post('/logout', adminController.logout);
 
 module.exports = router;
