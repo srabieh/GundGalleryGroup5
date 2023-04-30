0d6e87a228bd7d89892bf71134d3cf68721a3e23
Ricky Alvarez <alvarez1@kenyon.edu>
2023-04-30 19:27:44 +0000

    fixed

--- Makefile
+++ Makefile
@@ -1,10 +1,14 @@
 # Gund Gallery Immersive Experience
 # Makefile
 
+# Change for your system
+DB_USERNAME='root'
+DB_PASSWORD='newpassword'
+
 all: start
 
 install_deps:
 	npm install
 
 start:
-	export PORT=5000 JWT_SECRET=RICKYBOBBY && node server.js
+	export DB_USER=$(DB_USERNAME) DB_PASSWORD=$(DB_PASSWORD) PORT=5000 JWT_SECRET=RICKYBOBBY && node server.js
--- models/adminModel.js
+++ models/adminModel.js
@@ -1,43 +1,41 @@
 // models/userModel.js
-let db = require('../db');
+const db = require('../db');
 
 // ---- Admin class ----------------------------------------------------------------------
 class Admin {
-    constructor({ id, username }){
+    constructor({ id, username }) {
         this.id = id;
-        this.username = username; 
+        this.username = username;
     }
+    
+    // ---- Returns an Admin object ----------------------------------------------------------
+    static async login (username, password) {
+        try {
+            const result = await db.query('SELECT * FROM admins WHERE username = ?', [username]);
+    
+            if (result[0] && result[0].password === password) { // For simplicity, we're using plain-text password comparison. Use a proper hashing method in a real-world application.
+                let admin = new Admin(result[0]);
+                return admin;
+            } else if (result[0]) {
+                return 'Incorrect password';
+            } else {
+                return 'Incorrect username or password';
+            }
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+
+    async getAllWords() {
+        try {
+            const result = await db.query('SELECT * FROM words');
+            return result;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    };
 }
 
-// ---- Login (db auth) ----------------------------------------------------------------------------
-exports.login = async (username, password) => {
-    try {
-        const result = await db.query('SELECT * FROM admins WHERE username = ?', [username]);
-        
-        if (result[0] && result[0].password === password) { // For simplicity, we're using plain-text password comparison. Use a proper hashing method in a real-world application.
-			console.log(result[0]);
-            let admin = new Admin(result[0]);
-            return admin;
-        } else if (result[0]) {
-            return 'Incorrect password';
-		} else {
-            return 'Incorrect username or password';
-        }
-    } catch(error) {
-        console.error(error);
-        throw error;
-    }
-};
-
-// ---- getAllWords (from all installations) -------------------------------------------------------
-exports.getAllWords = async (admin) => {
-    try {
-        const result = await db.query('SELECT * FROM words');
-        return result;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
-};
-
-exports.Admin = Admin;
+module.exports = Admin;
