68be19b52c8fc87520c96d97e0fbcdf3b3e22023
Ricky Alvarez <alvarez1@kenyon.edu>
2023-05-04 17:48:01 +0000

    fixed wordcloud

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path = require('path');
 const jwt = require("jsonwebtoken");
 const router = express.Router();
 const cookieParser = require("cookie-parser");
+const fileUpload = require('express-fileupload');
+const Jimp = require("jimp");
+const fs = require('fs')
+const QrCode = require('qrcode-reader');
 
 const Client = require('./models/clientModel.js').Client;
 
@@ -22,6 +26,16 @@ app.use(cookieParser());
 
 // Serve static files
 app.use('/public', express.static(path.join(__dirname, 'public')))
+
+// File-upload
+app.use(
+    fileUpload({
+        limits: {
+            fileSize: 10000000,
+        },
+        abortOnLimit: true,
+    })
+);
 
 // Routes
 const adminRoutes = require('./routes/adminRoutes');
@@ -44,11 +58,33 @@ app.get('/words', (req, res) => {
     res.render('wordCloud');
 });
 
-app.post('/scan', (req, res) => {
+app.post('/scan', async (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+
     const { image } = req.files;
+    const buff = Buffer.from(image, 'base64');
 
     if (!image) { return res.status(400); }
-    
+    if (/^image/.test(image.mimetype)) return res.sendStatus(400);
+
+    // Parse the image using Jimp.read() method
+    await Jimp.read(buff, function (err, image) {
+        if (err) {
+            console.error(err);
+        }
+        // Creating an instance of qrcode-reader module
+        let qrcode = new qrCode();
+        qrcode.callback = function (err, value) {
+            if (err) {
+                console.error(err);
+            }
+            // Printing the decrypted value
+            print(value);
+            res.json(value);
+        };
+        // Decoding the QR code
+        qrcode.decode(image.bitmap);
+    });
 })
 
 app.get('/survey', (req, res) => {
