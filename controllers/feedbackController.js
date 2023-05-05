485c117d16da28198f3e7b69c6ebbe311057af7c
srabieh <rabieh1@kenyon.edu>
2023-05-05 01:40:13 +0000

    adding basic feedback page architecture

---
+++ controllers/feedbackController.js
@@ -0,0 +1,10 @@
+const jwt = require("jsonwebtoken");
+
+exports.index = async (req, res) => {
+	//load user's name ?
+	return res.status(200);
+}
+
+exports.submit = {
+	return res.redirect("/thanks");
+}
---
+++ routes/feedbackRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const feedbackController = require('../controllers/feedbackController');
+
+//add admin route handlers
+
+//gets
+router.get('/',feedbackController.index);
+
+//should this be post?  ...
+router.get('/thanks',feedbackController.thanks);
+
