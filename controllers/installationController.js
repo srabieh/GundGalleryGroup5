1e4523861d2ee2f3b09f72df3c16e44bf4731dc6
Ricky Alvarez <alvarez1@kenyon.edu>
2023-05-08 19:38:19 +0000

    mo stuff

--- controllers/installationController.js
+++ controllers/installationController.js
@@ -25,6 +25,7 @@ exports.get = async (req, res) => {
     res.redirect("/");
 }
 
+<<<<<<< HEAD
 //push user response on installation to the database.
 exports.pushWords = async (req, res) => {
 	const url = req.headers.referer;
@@ -45,3 +46,34 @@ exports.pushWords = async (req, res) => {
 	} 
 
 }
+=======
+exports.delete = async (req, res) => {
+    const { id } = req.params;
+    try {
+    const result = await db.query('DELETE FROM installations WHERE id = ?', [id]);
+    if (result.affectedRows === 1) {
+        return res.status(200).send({ message: `Installation with ID ${id} deleted successfully.` });
+    } else {
+        return res.status(404).send({ message: `Installation with ID ${id} not found.` });
+    }
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send({ message: 'Internal server error.' });
+    }
+}
+
+exports.create = async (req, res) => {
+    const { work_name, artist, material_medium, date, info_short_desc } = req.body;
+    try {
+        const result = await db.query('INSERT INTO installations (work_name, artist, material_medium, date, info_short_desc) VALUES (?, ?, ?, ?, ?)', [work_name, artist, material_medium, date, info_short_desc]);
+        if (result.affectedRows === 1) {
+            return res.status(201).send({ message: 'Installation created successfully.' });
+        } else {
+            return res.status(500).send({ message: 'Error occurred while creating the installation.' });
+        }
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send({ message: 'Internal server error.' });
+    }
+}
+>>>>>>> 19d5499 (mo stuff)
--- routes/installationRoutes.js
+++ routes/installationRoutes.js
@@ -2,6 +2,13 @@
 const express = require('express');
 const router = express.Router();
 const installationController = require('../controllers/installationController');
+const multer  = require('multer')
+
+const upload = multer({ dest: 'public/images/' });
+
+
+router.post("/", upload.single('image'), installationController.create);
+router.post("/delete", installationController.delete);
 
 //Getting
 router.get('/:id', installationController.get);
